5e59a0c1d0754cf2d577cfc3d79a667fcb3ba2bd
Willem Röpke <willemropke@hotmail.com>
2021-03-04 15:45:07 +0100

    Added category explorer

--- imperium_app.py
+++ imperium_app.py
@@ -1,25 +1,70 @@
 import argparse
-from preprocessing import preprocess
+import itertools
 import streamlit as st
 
 from md_templates import *
+from preprocessing import preprocess
 
+categories = {
+        'Professional consultancies/law firms/self-employed consultants': (
+            'Professional consultancies',
+            'Law firms',
+            'Self-employed consultants'
+        ),
+        'In-house lobbyists and trade/professional associations': (
+            'Companies & groups',
+            'Trade and business organisations',
+            'Trade unions and professional associations',
+            'Other in house lobbyists'
+        ),
+        'Non-governmental organisations': (
+            'Non-governmental organisations, platforms and networks and similar'
+        ),
+        'Think tanks, research and academic institutions': (
+            'Think tanks and research institutions',
+            'Academic institutions'
+        ),
+        'Organisations representing churches and religious communities': (
+            'Organisations representing churches and religious communities'
+        ),
+        'Organisations representing local, regional and municipal authorities, other public or mixed entities, etc.': (
+            'Regional structures',
+            'Other sub-national public authorities',
+            'Transnational associations and networks of public regional or other sub-national authorities',
+            'Other public or mixed entities, created by law whose purpose is to act in the public interest'
+        )
+    }
+
+all_categories = tuple(itertools.chain.from_iterable(categories.items()))
 
 def run(args):
     print('Running the Imperium app')
-    st.title('Imperium')
     data = preprocess(args.data)
-    st.markdown(start_page)
-    st.markdown(data_explorer)
-    st.dataframe(data)
-    st.line_chart(data)
 
-    # balloons: st.balloons()
-    # info message: st.info("Info message")
+    # Main template
+    st.markdown(start_template)
+
+    # Sidebar elements
     view_selectbox = st.sidebar.selectbox(
         "What would you like to see?",
-        ("Europe", "Sofyan")
+        ('Show me the map', 'Categories')
     )
+
+    data_explorer_button = st.sidebar.button('Explore the raw data!')
+
+    if data_explorer_button:
+        st.markdown(data_explorer_template)
+        st.dataframe(data)
+
+    if view_selectbox == 'Show me the map':
+        pass
+    elif view_selectbox == 'Categories':
+        st.markdown(categories_template)
+        category_selectbox = st.selectbox("Select category", tuple(categories.keys()))
+        if category_selectbox in categories.keys():
+            st.selectbox('Select subcategory', categories[category_selectbox])
+        st.markdown(compare_categories_template)
+        compare_multiselect = st.multiselect('Compare categories', all_categories)
 
 
 if __name__ == "__main__":
--- md_templates.py
+++ md_templates.py
@@ -1,7 +1,19 @@
-start_page = """
-## Looking at the EU
+start_template = """
+# Imperium: Looking at the EU
 """
 
-data_explorer = """
+data_explorer_template = """
 ### Explore our data in the table beneath!
+"""
+
+map_template = """
+### Explore our interactive map!
+"""
+
+categories_template = """
+### Explore our categories!
+"""
+
+compare_categories_template = """
+### Compare our categories!
 """
