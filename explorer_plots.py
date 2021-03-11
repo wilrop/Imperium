51ed667f73eae73cc279f7fb61a233ba0d814225
Sofyan Ajridi <ajridis@gmail.com>
2021-03-11 17:50:21 +0100

    Added chandle charts for lobbying cost per year

--- explorer_plots.py
+++ explorer_plots.py
@@ -1,4 +1,6 @@
 import plotly.express as px
+import plotly.graph_objects as go
+
 
 
 def explore_country(country_data):
@@ -10,6 +12,18 @@ def explore_category():
     return 0
 
 
+
+#Dummy data
+year = [2014,2015,2016,2017]
+lobbying_cost_low = [10000,20000,30000,40000]
+lobbying_cost_high = [50000,60000,50000,100000]
+
+
 def explore_business(business_data):
-    fig = px.bar(business_data, x='year', y='lobbying costs')
+    fig = go.Figure(data=[go.Candlestick(x=year,
+                       open=lobbying_cost_low, high=lobbying_cost_high,
+                       low=lobbying_cost_low, close=lobbying_cost_high)]
+                       )
+                       
+    fig.update_layout(xaxis_rangeslider_visible=False)
     return fig
