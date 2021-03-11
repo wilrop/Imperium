af841289abec584d5a09b60f84cd5408037cdace
Sofyan Ajridi <ajridis@gmail.com>
2021-03-11 18:11:52 +0100

    Connected candles graph with back end data

--- explorer_plots.py
+++ explorer_plots.py
@@ -11,18 +11,10 @@ def explore_country(country_data):
 def explore_category():
     return 0
 
-
-
-#Dummy data
-year = [2014,2015,2016,2017]
-lobbying_cost_low = [10000,20000,30000,40000]
-lobbying_cost_high = [50000,60000,50000,100000]
-
-
 def explore_business(business_data):
-    fig = go.Figure(data=[go.Candlestick(x=year,
-                       open=lobbying_cost_low, high=lobbying_cost_high,
-                       low=lobbying_cost_low, close=lobbying_cost_high)]
+    fig = go.Figure(data=[go.Candlestick(x=business_data['year'],
+                       open=business_data['begin_int'], high=business_data['end_int'],
+                       low=business_data['begin_int'], close=business_data['end_int'])]
                        )
                        
     fig.update_layout(xaxis_rangeslider_visible=False)
