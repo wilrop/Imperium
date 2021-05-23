4e16508714d4ae761ade0cf86e1ba6184911c4dd
unknown <sofyan.ajridi@vub.be>
2021-05-23 15:37:49 +0200

    Cleaned up imperium.app.py

--- imperium_app.py
+++ imperium_app.py
@@ -9,6 +9,7 @@ import explorer_plots
 import comparer_plots
 from data_loader import DataLoader
 
+# Bulma stylesheet
 external_stylesheets = [
     'https://cdn.jsdelivr.net/npm/bulma@0.9.2/css/bulma.min.css']
 
@@ -30,62 +31,51 @@ world_map = world_plots.map_plot(
     iso3_codes, countries_business_amount, countries_list)
 
 
-def aboutpage():
-    layout = html.Div([
-        nav,
-        about
-    ], style={'backgroundColor': '$light'})
-    return layout
 
 
-def homepage():
-    layout = html.Div([
-        nav,
-        body
-    ], style={'backgroundColor': '$light'})
-    return layout
+################# Start Callback handlers #################
 
-
-@app.callback(Output('info-here', 'children'),
-              Output('info-here-2', 'children'),
-              Output('country-here-3', 'children'),
+# Update information cards with numbers of selected country/category/organisation
+@app.callback(Output('info-card', 'children'),
+              Output('info-card-2', 'children'),
+              Output('selected-country', 'children'),
               [Input('countries-dropdown', 'value'),
                Input('organisations-dropdown', 'value'),
                Input('sub-categories-dropdown', 'value')])
 def update_companies_here(country, organisation, sub_category):
-    info_here = "**" + "No criteria selected" + "**"
-    info_here_2 = "**" + "No criteria selected" + "**"
-    country_here = "**" + "No criteria selected" + "**"
+    info_card = "**" + "No criteria selected" + "**"
+    info_card_2 = "**" + "No criteria selected" + "**"
+    selected_country = "**" + "No criteria selected" + "**"
     if country is not None:
         country_data = data.get_countries_data([country])
         company_amount = str(len(country_data))
         ep_amount = str(country_data['EP passes'].sum())
-        info_here = "**" + country + "**" + " has " + \
+        info_card = "**" + country + "**" + " has " + \
             company_amount + " organisations in our database"
-        info_here_2 = "**" + country + "**" + " has " + ep_amount + " EP Passes"
-        country_here = country
+        info_card_2 = "**" + country + "**" + " has " + ep_amount + " EP Passes"
+        selected_country = country
     elif organisation is not None:
-        country_here = organisation
+        selected_country = organisation
         business_data = data.get_organisations_data([organisation])
         ep_passes = 0
         lobbyist = 0
         for row in business_data.itertuples(index=False):
             ep_passes += row[business_data.columns.get_loc('EP passes')]
             lobbyist += row[business_data.columns.get_loc('lobbyists (FTE)')]
-        info_here = "**" + organisation + "**" + \
+        info_card = "**" + organisation + "**" + \
             " has " + str(lobbyist) + " lobbyists"
-        info_here_2 = "**" + organisation + "**" + \
-                    " has " + str(ep_passes) + " EP Passes"
+        info_card_2 = "**" + organisation + "**" + \
+            " has " + str(ep_passes) + " EP Passes"
     elif sub_category is not None:
-        country_here = sub_category
+        selected_country = sub_category
         business_data = data.get_sub_categories_data([sub_category])
         nr_organisations = str(len(business_data))
         ep_passes = str(business_data['EP passes'].sum())
-        info_here = "There are " + nr_organisations + \
+        info_card = "There are " + nr_organisations + \
             " **" + sub_category + "**" + " in our database"
-        info_here_2 = "**" + sub_category + "**" + " has " + ep_passes + " EP Passes"
+        info_card_2 = "**" + sub_category + "**" + " has " + ep_passes + " EP Passes"
 
-    return info_here, info_here_2, country_here
+    return info_card, info_card_2, selected_country
 
 
 # Update the explore plots.
@@ -95,7 +85,7 @@ def update_companies_here(country, organisation, sub_category):
                Input('sub-categories-dropdown', 'value')])
 def update_explore_plot(country, organisation, sub_category):
     ctx = dash.callback_context
-    
+
     if ctx.triggered:
         dropdown = ctx.triggered[0]['prop_id'].split('.')[0]
         if dropdown == 'countries-dropdown':
@@ -104,7 +94,8 @@ def update_explore_plot(country, organisation, sub_category):
             return country_plot
         elif dropdown == 'organisations-dropdown':
             organisation_data = data.get_organisations_data([organisation])
-            organisation_plot = explorer_plots.explore_organisation(organisation_data)
+            organisation_plot = explorer_plots.explore_organisation(
+                organisation_data)
             return organisation_plot
         else:
             category_data = data.get_sub_categories_data([sub_category])
@@ -112,7 +103,7 @@ def update_explore_plot(country, organisation, sub_category):
             return category_plot
     else:
         # This data will the always be empty
-        category_data = data.get_countries_data([])  
+        category_data = data.get_countries_data([])
         category_plot = explorer_plots.explore_category(category_data)
         return category_plot
 
@@ -144,7 +135,8 @@ def dropdown_map_interaction(country, organisation, sub_category, click_data):
                 if country is None:
                     return country, None, None, world_map, None
                 else:
-                    zoomed_world_map = world_plots.zoom_world_map(world_map, country)
+                    zoomed_world_map = world_plots.zoom_world_map(
+                        world_map, country)
                     return country, None, None, zoomed_world_map, None
             elif dropdown == 'organisations-dropdown':
                 return None, organisation, None, world_map, None
@@ -200,31 +192,30 @@ def update_compare_dropdown(country, organisation, sub_category):
     else:
         return 'tab-country'
 
+################# End Callback handlers #################
 
-# App layout
+################# Start Main HTML Code #################
+
+# Nav bar HTML
 nav = html.Nav([
     html.Div([
         html.A([
             dcc.Markdown(children="IMPERIUM", className='title is-5 center'),
-
         ], className="navbar-item")
-
     ], className="navbar-brand"),
     html.Div([
         html.Div([
             html.A([
                 "Home"
-
             ], className="navbar-item", href="/home"),
             html.A([
                 "About"
-
             ], className="navbar-item", id="about", href="/about")
-
         ], className="navbar-start")
     ], className="navbar-menu")
-
 ], className="navbar is-light has-shadow")
+
+# Main dashboard HTML
 body = html.Div([
     html.Br(),
     html.Br(),
@@ -249,14 +240,12 @@ body = html.Div([
                              style={'margin-right': '5px'}),
             ], className='column')
         ], className='columns is-centered')
-
     ], className='card'),
     html.Br(),
     html.Div([
         html.Div([
             html.Div([
                 dcc.Graph(id='world-map', figure=world_map)
-
             ], className='card'),
         ], className='column is-two-thirds'),
         html.Div([
@@ -265,7 +254,7 @@ body = html.Div([
                     html.Br(),
                     html.Br(),
                     dcc.Markdown(className='subtitle is-3 center',
-                                 id='info-here'),
+                                 id='info-card'),
                 ], className='content is-centered')
             ], className='card meta-info'),
             html.Br(),
@@ -275,7 +264,7 @@ body = html.Div([
                     html.Br(),
                     html.Br(),
                     dcc.Markdown(className='subtitle is-3 center',
-                                 id='info-here-2'),
+                                 id='info-card-2'),
                 ], className='content is-centered')
             ], className='card meta-info'),
         ], className='column'),
@@ -286,12 +275,10 @@ body = html.Div([
                 dcc.Markdown(children='You are looking at ', className='subtitle is-4 center',
                              style={'border-radius': '60px'}),
                 dcc.Markdown(className='title is-4 center',
-                             id='country-here-3'),
+                             id='selected-country'),
                 dcc.Graph(id='explore-plot')
             ], className='column is-half'),
             html.Div([
-                # dcc.Markdown(children='Add more items to compare their lobbying practices over time!',
-                #             className='subtitle is-4 center', style={'border-radius': '60px'}),
                 dcc.Tabs(id='compare-tabs', value='tab-country', children=[
                     dcc.Tab(label='Compare countries', value='tab-country', children=[
                         dcc.Dropdown(id='compare-countries-dropdown', options=countries, multi=True,
@@ -315,22 +302,50 @@ body = html.Div([
 ], className='container is-fluid', id="home")
 
 
+# About page HTML 
 about = html.Div([html.Div([
     html.Br(),
     html.Div([html.Div([
         dcc.Markdown(md_templates.about_page)
-
-
     ], className="content is-normal")])
 ], className="container")])
 
 
+
+################# End Main HTML Code ##################
+
+# Load about page
+def aboutpage():
+    layout = html.Div([
+        nav,
+        about
+    ], style={'backgroundColor': '$light'})
+    return layout
+
+#Load homepage
+def homepage():
+    layout = html.Div([
+        nav,
+        body
+    ], style={'backgroundColor': '$light'})
+    return layout
+
+
+# Url handler
 app.layout = html.Div([
     dcc.Location(id='url', refresh=False),
     html.Div(id='page-content')
 ])
 
+# App Header settings, loads favicon image
+app.head = [
+    html.Link(
+        href='/assets/logo2.jpg',
+        rel='icon'
+    ),
+]
 
+# Responsible for handling URL and loading correct page depending on which url is accesed.
 @app.callback(dash.dependencies.Output('page-content', 'children'),
               [dash.dependencies.Input('url', 'pathname')])
 def display_page(pathname):
@@ -341,13 +356,6 @@ def display_page(pathname):
     else:
         return homepage()
 
-
-app.head = [
-    html.Link(
-        href='/assets/logo2.jpg',
-        rel='icon'
-    ),
-]
 
 if __name__ == "__main__":
     app.run_server(debug=True)
